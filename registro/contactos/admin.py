9a78a2324b04e2d56f45edff192ba73dfa3ff457
Hector Torres <hectormtc@live.com>
2020-03-09 17:19:44 -0600

    Add files via upload

--- registro/contactos/admin.py
+++ registro/contactos/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Contacto,Direccion
+from .models import Contacto,Direccion, Medio,Categoria
 
+admin.site.register(Medio)
+admin.site.register(Categoria)
 admin.site.register(Contacto)
 admin.site.register(Direccion)
--- registro/contactos/models.py
+++ registro/contactos/models.py
@@ -2,14 +2,24 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 import datetime
 
+class Medio(models.Model):
+    medio = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.medio
+
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.categoria
 
 class Direccion(models.Model):
     direccion = models.CharField(
         ("Direccion"), max_length=255, blank=True, null=True)
 
     def __str__(self):
-        return self.city if self.city else ""
-
+        return self.direccion
 
 class Contacto(models.Model):
     nombre = models.CharField(max_length=255)
@@ -19,8 +29,19 @@ class Contacto(models.Model):
     direccion = models.ForeignKey(
         'Direccion',on_delete=models.CASCADE, blank=True, null=True)
     fecha = models.DateField("Fecha", null=True, blank=True, help_text="Fecha en que recibio")
-    hora = models.TimeField(['Hora:Minuto'], blank=True, null=True)
-    now = datetime.datetime.now()
+    hora = models.TimeField("Hora:Minuto", blank=True, null=True)
+    descripcion = models.TextField(help_text="Descripcion sobre el contacto y pedido", blank=True)
+
+    COMPLETADO = 'C'
+    PROCESO = 'P'
+    ESTADOS = ((COMPLETADO,'Completado'),(PROCESO,'En proceso'))
+    estado = models.CharField(max_length=2, choices=ESTADOS, default=PROCESO)
+
+    orden  = models.PositiveIntegerField(help_text="Numero de Orden", blank=True)
+
+    medio = models.ForeignKey('Medio', on_delete=models.SET_NULL, null=True)
+    categoria = models.ForeignKey('Categoria', on_delete=models.SET_NULL, null=True)
+    email = models.EmailField(unique=True)
 
 
     def __str__(self):
